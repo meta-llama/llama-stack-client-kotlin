d2e6f3bcf7ff86b0ab5842b5ce5ac670f95ded5a
cmodi-meta <98582575+cmodi-meta@users.noreply.github.com>
2024-11-07 15:20:40 -0800

    Add response in ChatCompletion

--- llama-stack-client-kotlin-client-local/build.gradle.kts
+++ llama-stack-client-kotlin-client-local/build.gradle.kts
@@ -9,16 +9,3 @@ dependencies {
     implementation(fileTree("libs") { include("*.jar") })
     implementation(files("jni/**/*.so"))
 }
-tasks {
-    jar {
-        from("jni/arm64-v8a") {
-            into("jni/arm64-v8a")
-        }
-        from("jni/x86_64") {
-            into("jni/x86_64")
-        }
-        from("libs") {
-            into("libs")
-        }
-    }
-}
--- llama-stack-client-kotlin-client-local/src/main/kotlin/com/llama_stack_client/api/client/local/InferenceServiceLocalImpl.kt
+++ llama-stack-client-kotlin-client-local/src/main/kotlin/com/llama_stack_client/api/client/local/InferenceServiceLocalImpl.kt
@@ -3,6 +3,7 @@
 package com.llama_stack_client.api.client.local
 
 import com.llama_stack_client.api.core.RequestOptions
+import com.llama_stack_client.api.models.CompletionMessage
 import com.llama_stack_client.api.models.InferenceChatCompletionParams
 import com.llama_stack_client.api.models.InferenceChatCompletionResponse
 import com.llama_stack_client.api.models.InferenceCompletionParams
@@ -15,12 +16,21 @@ class InferenceServiceLocalImpl
 constructor(
     private val clientOptions: LocalClientOptions,
 ) : InferenceService, LlamaCallback {
+
+    private var resultMessage: String = ""
+    private var onResultComplete: Boolean = false
+
     override fun onResult(p0: String?) {
-        println("onResult" + p0)
+        println("onResult is: $p0")
+        resultMessage += p0
+
+        if (p0.equals("<|eot_id|>")) {
+            onResultComplete = true
+        }
     }
 
     override fun onStats(p0: Float) {
-        println("onStats" + p0)
+        println("I'm in onStats but not doing anything on purpose for now")
     }
 
     override fun embeddings(): EmbeddingService {
@@ -32,37 +42,30 @@ constructor(
         requestOptions: RequestOptions
     ): InferenceChatCompletionResponse {
         val mModule = clientOptions.llamaModule
+        val promptExample =
+            "<|begin_of_text|><|start_header_id|>system<|end_header_id|><|eot_id|><|start_header_id|>user<|end_header_id|>what is France's capital?<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+        /*val promptExample =
+            "<|begin_of_text|><|start_header_id|>user<|end_header_id|>What is France's capital?<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+        */
+        println("Prompt is: $promptExample")
+        mModule.generate(promptExample, ((promptExample.length * 0.75) + 64).toInt(), this, false)
 
-        mModule.generate("what is the capital of France", 64, this, false)
+        while (!onResultComplete) {
+            Thread.sleep(2)
+        }
+        onResultComplete = false
+        println("Response is: $resultMessage")
 
-        //        val request =
-        //            HttpRequest.builder()
-        //                .method(HttpMethod.POST)
-        //                .addPathSegments("inference", "chat_completion")
-        //                .putAllQueryParams(clientOptions.queryParams)
-        //                .putAllQueryParams(params.getQueryParams())
-        //                .putAllHeaders(clientOptions.headers)
-        //                .putAllHeaders(params.getHeaders())
-        //                .body(json(clientOptions.jsonMapper, params.getBody()))
-        //                .build()
-        //        return clientOptions.httpClient.execute(request, requestOptions).let { response ->
-        //            response
-        //                .use { chatCompletionHandler.handle(it) }
-        //                .apply {
-        //                    if (requestOptions.responseValidation ?:
-        // clientOptions.responseValidation) {
-        //                        validate()
-        //                    }
-        //                }
-        //        }
+        // mModule.generate("What is the capital of France?", 64, this, false)
+        val content = CompletionMessage.Content.ofString(resultMessage)
+        val completionMessage = CompletionMessage.builder().content(content).build()
+
         return InferenceChatCompletionResponse.ofChatCompletionResponse(
-            InferenceChatCompletionResponse.ChatCompletionResponse.builder().build()
+            InferenceChatCompletionResponse.ChatCompletionResponse.builder()
+                .completionMessage(completionMessage)
+                .build()
         )
     }
-
-    //    private val completionHandler: Handler<InferenceCompletionResponse> =
-    //        jsonHandler<InferenceCompletionResponse>(clientOptions.jsonMapper)
-    //            .withErrorHandler(errorHandler)
 
     override fun completion(
         params: InferenceCompletionParams,
