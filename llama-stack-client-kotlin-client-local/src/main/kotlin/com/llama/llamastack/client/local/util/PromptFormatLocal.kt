5d60b9059980a8c26896d7589907cdaf1e3bc213
cmodi-meta <98582575+cmodi-meta@users.noreply.github.com>
2024-11-27 01:47:16 -0800

    Refactor conversation history to use CompletionMessage

--- llama-stack-client-kotlin-client-local/src/main/kotlin/com/llama/llamastack/client/local/util/PromptFormatLocal.kt
+++ llama-stack-client-kotlin-client-local/src/main/kotlin/com/llama/llamastack/client/local/util/PromptFormatLocal.kt
@@ -1,5 +1,6 @@
 package com.llama.llamastack.client.local.util
 
+import com.llama.llamastack.models.CompletionMessage
 import com.llama.llamastack.models.InferenceChatCompletionParams
 import com.llama.llamastack.models.SystemMessage
 import com.llama.llamastack.models.UserMessage
@@ -8,10 +9,6 @@ object PromptFormatLocal {
     private const val SYSTEM_PLACEHOLDER: String = "{{ system_prompt }}"
     private const val USER_PLACEHOLDER: String = "{{ user_prompt }}"
     private const val ASSISTANT_PLACEHOLDER: String = "{{ assistant_response }}"
-
-    private const val ROLE_KEY: String = "header_id"
-    private const val ROLE_USER: String = "user"
-    private const val ROLE_ASSISTANT: String = "assistant"
 
     private fun getSystemPromptTemplate(modelName: String?): String {
         return when (modelName) {
@@ -63,33 +60,40 @@ object PromptFormatLocal {
         var format: String = ""
 
         for (message in messages) {
-            if (message.isSystemMessage()) {
-                // system message. Expecting this will only called once
-                val systemMessage: SystemMessage? = message.systemMessage()
-                val content: String? = systemMessage?.content()?.string()
-                if (content != null) {
-                    formattedPrompt =
-                        getSystemPromptTemplate(modelName).replace(SYSTEM_PLACEHOLDER, content) +
-                            formattedPrompt
+            println("Message is: $message")
+            when {
+                message.isSystemMessage() -> {
+                    // system message. Expecting this will only called once
+                    val systemMessage: SystemMessage? = message.systemMessage()
+                    val content: String? = systemMessage?.content()?.string()
+                    if (content != null) {
+                        formattedPrompt =
+                            getSystemPromptTemplate(modelName)
+                                .replace(SYSTEM_PLACEHOLDER, content) + formattedPrompt
+                    }
                 }
-            } else if (message.isUserMessage()) {
-                // user or assistant
-                val userMessage: UserMessage? = message.userMessage()
-                val role = userMessage?._additionalProperties()?.get(ROLE_KEY)?.toString()
-                val content: String? = userMessage?.content()?.string()
-                if (content != null) {
-                    // order needs to be UserPrompt A, AssistantResponse A, UserPrompt B,
-                    // AssistantResponse B, ...
-                    if (role == ROLE_USER) {
+                message.isUserMessage() -> {
+                    // user message
+                    val userMessage: UserMessage? = message.userMessage()
+                    val content: String? = userMessage?.content()?.string()
+                    println("content is $content")
+                    if (content != null) {
                         format = getUserPromptTemplate(modelName).replace(USER_PLACEHOLDER, content)
-                    } else if (role == ROLE_ASSISTANT) {
+                    }
+                }
+                message.isCompletionMessage() -> {
+                    // assistant message
+                    val completionMessage: CompletionMessage? = message.completionMessage()
+                    val content: String? = completionMessage?.content()?.string()
+                    if (content != null) {
                         format =
                             getAssistantPromptTemplate(modelName)
                                 .replace(ASSISTANT_PLACEHOLDER, content)
                     }
                 }
-                formattedPrompt += format
             }
+            println("format is $format")
+            formattedPrompt += format
         }
 
         return formattedPrompt
