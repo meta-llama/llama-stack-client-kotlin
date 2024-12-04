487e34e545ac5d4cc3d9f72614d2a99ec8fd95c4
cmodi-meta <98582575+cmodi-meta@users.noreply.github.com>
2024-12-04 15:33:56 -0800

    Modify Download Script to Download to Lib directory in App

--- llama-stack-client-kotlin-client-local/download-prebuilt-et-lib.sh
+++ llama-stack-client-kotlin-client-local/download-prebuilt-et-lib.sh
@@ -6,17 +6,22 @@
 # LICENSE file in the root directory of this source tree.
 
 # Purpose: The goal of this script is to get ExecuTorch AAR file for local inferencing.
+# How to run:
+# - `sh download-prebuild-et-lib.sh` : download ExecuTorch AAR in apps/libs (meant to be run in Android App environment)
+# - `sh download-prebuild-et-lib.sh` : download+unzip ExecuTorch AAR in working directory and clean-up.
 # Based on ExecuTorch Commit ID: 0a12e33d22a3d44d1aa2af5f0d0673d45b962553
 
 set -eu
 
 AAR_URL="https://ossci-android.s3.amazonaws.com/executorch/release/executorch-20241202/executorch.aar"
 AAR_SHASUM_URL="https://ossci-android.s3.amazonaws.com/executorch/release/executorch-20241202/executorch.aar.sha256sums"
+LIBS_PATH="$(dirname "$0")/app/libs"
 UNZIP=false
 while [[ $# -gt 0 ]]; do
     case $1 in
         --unzip)
             UNZIP=true
+            LIBS_PATH="$(dirname "$0")"
             shift
             ;;
         *)
@@ -26,9 +31,14 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+if [ "$UNZIP" = false ]; then
+    mkdir -p "${LIBS_PATH}"
+fi
+
 curl -O "${AAR_SHASUM_URL}"
-shasum --check --status executorch.aar.sha256sums || curl "${AAR_URL}" -o executorch.aar
+shasum --check --status executorch.aar.sha256sums || curl "${AAR_URL}" -o "${LIBS_PATH}/executorch.aar"
 
 if [ "$UNZIP" = true ]; then
     unzip -q executorch.aar
+    rm "executorch.aar" "executorch.aar.sha256sums"
 fi
