8da636b6c068a70d76cc2b276339d8eaa0e4b5e7
cmodi-meta <98582575+cmodi-meta@users.noreply.github.com>
2024-12-04 17:12:35 -0800

    Clean settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,4 @@ rootProject.name = "llama-stack-client-kotlin-root"
 include("llama-stack-client-kotlin")
 include("llama-stack-client-kotlin-client-okhttp")
 include("llama-stack-client-kotlin-core")
-include("llama-stack-client-kotlin-example")
 include("llama-stack-client-kotlin-client-local")
