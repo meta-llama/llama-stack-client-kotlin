c76eb0ef501f6ab94f1a00bc616060e44cb002ee
Riandy <riandy@windowslive.com>
2025-03-19 10:44:19 -0700

    Disable dokka generation
    This is failing on gradle publish, hence disabling it for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.dokka") version "2.0.0"
+//    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 allprojects {
@@ -8,12 +8,12 @@ allprojects {
 }
 
 subprojects {
-    apply(plugin = "org.jetbrains.dokka")
+//    apply(plugin = "org.jetbrains.dokka")
 }
 
 // Avoid race conditions between `dokkaHtmlCollector` and `dokkaJavadocJar` tasks
-tasks.named("dokkaHtmlCollector").configure {
-    subprojects.flatMap { it.tasks }
-        .filter { it.project.name != "llama-stack-client-kotlin" && it.name == "dokkaJavadocJar" }
-        .forEach { mustRunAfter(it) }
-}
+//tasks.named("dokkaHtmlCollector").configure {
+//    subprojects.flatMap { it.tasks }
+//        .filter { it.project.name != "llama-stack-client-kotlin" && it.name == "dokkaJavadocJar" }
+//        .forEach { mustRunAfter(it) }
+//}
